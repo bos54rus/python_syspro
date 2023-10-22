5ca4e250b7d953f4cd65591cb5e79c712eb049ce
Zhukov Dmitrii <nonoi3345@mail.ru>
2023-10-22 20:16:59 +0700

    PEP8

--- mini5.py
+++ mini5.py
@@ -1,9 +1,13 @@
 def specialize(f, *args, **kwargs):
     def partial_func(*more_args, **more_kwargs):
         return f(*args, *more_args, **kwargs, **more_kwargs)
+
     return partial_func
+
+
 def sum(x, y):
     return x + y
+
 
 plus_one = specialize(sum, y=1)
 just_two = specialize(sum, 1, 1)
