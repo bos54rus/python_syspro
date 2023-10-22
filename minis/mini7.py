829dc0d9ddd1ab9e15b6731b24116d8ea5217cf0
Zhukov Dmitrii <nonoi3345@mail.ru>
2023-10-23 00:45:05 +0700

    mini7

--- minis/mini7.py
+++ minis/mini7.py
@@ -1,12 +1,11 @@
-def flatten(lst, depth=None):
+def flatten(lst, depth=float("inf")):
     result = []
     for item in lst:
-        if depth is None or depth > 0:
-            if isinstance(item, list):
-                result.extend(flatten(item, depth=None if depth is None else depth - 1))
-            else:
-                result.append(item)
+        if isinstance(item, list) and depth != 0:
+            result.extend(flatten(item, depth - 1))
         else:
             result.append(item)
     return result
+
+
 print(flatten([1, 2, [4, 5], [6, [7]], 8], depth=1))
