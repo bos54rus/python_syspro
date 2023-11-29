49d3beaafbe682c2aa88df1c64e109745fd54c75
Zhukov Dmitrii <75932292+bos54rus@users.noreply.github.com>
2023-11-29 23:33:22 +0700

    mini12_2.py

--- minis/mini12_2.py
+++ minis/mini12_2.py
@@ -1,9 +1,7 @@
-import itertools
-
 def chain(*iterables):
     for it in iterables:
         yield from it
 
 my_list = [42, 13, 7]
-result = list(itertools.chain([1, 2, 3], ['a', 'b'], my_list))
+result = list(chain([1, 2, 3], ['a', 'b'], my_list))
 print(result)
